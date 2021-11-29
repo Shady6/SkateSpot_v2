e0b3991f5c4d15e1f1f3fda067b51d7c7974f027
Shady06 <mikolaj.mikolaj@gmail.com>
2021-11-29 20:38:46 +0100

    Fix bug with slider not wanting to slide, while used over map

--- Frontend/skate_spot.frontend/src/hooks/map/disablePropagationInMap.ts
+++ Frontend/skate_spot.frontend/src/hooks/map/disablePropagationInMap.ts
@@ -1,14 +1,22 @@
 import L from 'leaflet'
 import { useEffect } from 'react'
+import { useMap } from 'react-leaflet'
 
 export const useDisablePropagationInMap = ({
   ref,
 }: {
   ref: React.MutableRefObject<HTMLElement | null>
 }) => {
+  const map = useMap()
+
   useEffect(() => {
     if (!ref.current) return
     L.DomEvent.disableScrollPropagation(ref.current)
-    L.DomEvent.disableClickPropagation(ref.current)
+    ref.current.onmouseenter = e => map.dragging.disable()
+    ref.current.onmouseleave = e => map.dragging.enable()
+
+    return () => {
+      map.dragging.enable()
+    }
   }, [ref])
 }
